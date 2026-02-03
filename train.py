c9f8cf4e4a0ab7024c20908c04ecba96caaaa0bb
Mohid Khan <mohidkhan169@gmail.com>
2026-02-02 18:36:32 -0800

    using tensor for the first time

--- train.py
+++ train.py
@@ -17,3 +17,8 @@ decode = lambda l: "".join([itos[i] for i in l])
 
 print(encode("hii there"))
 print(decode(encode("hii there")))
+
+import torch
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000])
