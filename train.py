b2a60da537cbf4e7cc016d2de97cf32c92cc2926
Mohid Khan <mohidkhan169@gmail.com>
2026-02-02 18:30:12 -0800

    basic tokenizeer and encoding

---
+++ train.py
@@ -0,0 +1,19 @@
+with open('input.txt', 'r') as file:
+    text = file.read()
+
+print(len(text))
+
+# create a mapping of unique characters to integers
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print(chars)
+print(vocab_size)
+
+stoi = {ch:i for i, ch in enumerate(chars)}
+itos = {i:ch for i, ch in enumerate(chars)}
+
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+print(encode("hii there"))
+print(decode(encode("hii there")))
